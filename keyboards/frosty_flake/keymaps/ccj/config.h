8468ca828562f674d414504e3545dc777969462f
Corey Jewett <cj@syntheticplayground.com>
2018-09-24 17:53:27 -0700

    starter mods from default.

---
+++ keyboards/frosty_flake/keymaps/ccj/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+
+// https://beta.docs.qmk.fm/features/feature_mouse_keys
+#define MOUSEKEY_DELAY             300
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+#endif
