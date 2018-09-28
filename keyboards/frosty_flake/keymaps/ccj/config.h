f00b439be7660991f119f536efbf31389f19b288
Corey Jewett <cj@syntheticplayground.com>
2018-09-27 19:06:39 -0700

    faster

--- keyboards/frosty_flake/keymaps/ccj/config.h
+++ keyboards/frosty_flake/keymaps/ccj/config.h
@@ -6,10 +6,10 @@
 // place overrides here
 
 // https://beta.docs.qmk.fm/features/feature_mouse_keys
-#define MOUSEKEY_DELAY             300
-#define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         10
-#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_DELAY             100
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_TIME_TO_MAX       10
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
