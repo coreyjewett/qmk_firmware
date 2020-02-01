141869a6aa2b22ef02514e865396387cd406d158
Corey Jewett <cj@syntheticplayground.com>
2020-01-31 23:38:11 -0800

    Add Tap/Hold to Enter; add Chrome versions of Caps & Enter

--- keyboards/frosty_flake/keymaps/ccj/keymap.c
+++ keyboards/frosty_flake/keymaps/ccj/keymap.c
@@ -10,6 +10,7 @@
  * Default Layer (_BASE) is vanilla QWERTY with the following twiddles:
  *   + modifier key locations match typical Apple keyboard
  *   + Caps Lock is: Tap ESC, Hold CTRL
+ *   + Enter is: Tap ENT, Hold CTRL
  *   + Space Cadet Shifts
  *   + Layer Tap on Space (hold for _MOUSE layer)
  *   + Layer Tap on Tab (hold for _NUMPAD layer)
@@ -21,6 +22,9 @@
 
 #define F_NUMPAD LT(_NUMPAD, KC_F)
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
+#define ALT_ESC MT(MOD_LALT, KC_ESC)
+#define CTL_ENT MT(MOD_LCTL, KC_ENT)
+#define ALT_ENT MT(MOD_LALT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FN] = KEYMAP_TKL(\
@@ -30,6 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______,    _______,  _______,                _______, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,            _______, \
   _______,  _______,  _______,                      _______,                           _______,    _______,    _______,   _______,                 _______, _______, _______  ),
+
 /* start of WASD
  *  [_MOUSE] = KEYMAP_TKL(\
  * _______,      _______,  _______,  _______,  _______,      _______,  _______,  _______,  _______,        _______,  _______,  _______,  _______,   _______,  _______,  _______, \
@@ -62,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_WSCH,      _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______,   _______,  _______,  _______, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,      _______,   _______,  _______,  _______, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,      _______,   _______,  _______,  _______, \
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______,    _______,  _______,                _______, \
+  ALT_ESC,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, _______,    _______,  _______,                ALT_ENT, \
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,            _______, \
   _______,  KC_LCTL,  KC_LALT,                      _______,                           _______,    _______,    _______,   _______,                 _______, _______, _______  ),
 
@@ -70,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,       KC_F1,    KC_F2,    KC_F3,    KC_F4,        KC_F5,    KC_F6,    KC_F7,    KC_F8,          KC_F9,    KC_F10,   KC_F11,   KC_F12,    KC_PSCR, KC_SLCK, TG(_CHROMEOS), \
   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,       KC_BSPC,   KC_INS,  KC_HOME,  KC_PGUP, \
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,      KC_BSLS,   KC_DEL,  KC_END,   KC_PGDN, \
-  CTL_ESC,  KC_A,     KC_S,     KC_D,     F_NUMPAD, KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                KC_ENT, \
+  CTL_ESC,  KC_A,     KC_S,     KC_D,     F_NUMPAD, KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                CTL_ENT, \
   KC_LSPO,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                KC_RSPC,            KC_UP, \
   KC_LCTL,  KC_LALT,  KC_LGUI,              LT(_MOUSE, KC_SPC),                        KC_RGUI,       KC_RALT,    KC_APP,    KC_RCTL,              KC_LEFT, KC_DOWN,  KC_RGHT  )
 };
@@ -86,5 +91,5 @@ uint32_t layer_state_set_user(uint32_t state) {
         led_set_user(0);
         break;
     }
-  return state;
+    return state;
 }
