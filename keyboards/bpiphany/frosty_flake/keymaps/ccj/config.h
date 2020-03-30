c94286a0561d4cb00546cd38b5a145fea01d167a
Corey Jewett <cj@syntheticplayground.com>
2020-03-29 20:33:57 -0700

    shift+z isn't dumb anymore.

--- keyboards/bpiphany/frosty_flake/keymaps/ccj/config.h
+++ keyboards/bpiphany/frosty_flake/keymaps/ccj/config.h
@@ -9,3 +9,6 @@
 #define MOUSEKEY_TIME_TO_MAX       10
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold
+#define PERMISSIVE_HOLD
