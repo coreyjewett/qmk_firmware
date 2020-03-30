b5f552bdbb2bce9d421beb2e9575c83bedec602b
Corey Jewett <cj@syntheticplayground.com>
2020-03-29 20:06:25 -0700

    Actually use the current built file.

--- write_frosty.sh
+++ write_frosty.sh
@@ -3,12 +3,17 @@
 set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+src=bpiphany_frosty_flake_ccj.hex
 
 cd $DIR
 echo "Wave the magnet around the arrow pad within the next 5 seconds."
 sleep 5
 
+if [ ! -e $src; ]; then
+  qmk compile -kb bpiphany/frosty_flake -km ccj
+fi
+
 sudo dfu-programmer atmega32u2 erase \
-  && sudo dfu-programmer atmega32u2 flash .build/frosty_flake_ccj.hex \
+  && sudo dfu-programmer atmega32u2 flash $src \
   && sudo dfu-programmer atmega32u2 reset \
-  && mv .build/frosty_flake_ccj.hex history/frosty_flake_ccj.hex.$(git rev-parse --short HEAD)-$(date +%y%m%d_%H%M%S)
+  && mv $src history/$src.$(git rev-parse --short HEAD)-$(date +%y%m%d_%H%M%S)
