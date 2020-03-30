f7d8d5667027f5ec56cded01006db7a6c557de26
Corey Jewett <cj@syntheticplayground.com>
2020-03-29 20:23:26 -0700

    auto-compile working

--- write_frosty.sh
+++ write_frosty.sh
@@ -6,12 +6,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 src=bpiphany_frosty_flake_ccj.hex
 
 cd $DIR
-echo "Wave the magnet around the arrow pad within the next 5 seconds."
-sleep 5
-
-if [ ! -e $src; ]; then
+if [ ! -e $src ]; then
+  echo "Have to compile it first."
   qmk compile -kb bpiphany/frosty_flake -km ccj
 fi
+
+echo "Wave the magnet around the arrow pad within the next 5 seconds."
+sleep 5
 
 sudo dfu-programmer atmega32u2 erase \
   && sudo dfu-programmer atmega32u2 flash $src \
